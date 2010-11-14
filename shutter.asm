e754378df2b42fb3278d1cd81d55b254483e9775
W1N9Zr0 <wingzr0@gmail.com>
2010-11-13 19:20:44 -0500

    manual mode works

--- shutter.asm
+++ shutter.asm
@@ -14,114 +14,50 @@ Fosc equ 8 * 1000 * 1000
 
 	#include shutter.inc
 
-
-
-interupt
-	movfw TimerMode
-	TableLookup
-	goto DelayCountdown
-	goto ExposureCountdown
-
-DelayCountdown
-		movlw timerw
-		call BD3Dec
-	
-		movlw timerw
-		call BD3Test
-		btfss STATUS, Z
-			goto NonZeroCountDown
-			
-		
-		bsf ShutterState, SHUTTER_CLICK
-		bsf TimerMode, 0
-		
-		movlw exposurew
-		movwf DisplayPointer
-		
-		Copy3 timerw, timer
-	goto DoneInterrupt
-	
-NonZeroCountDown
-	 	movfw timerw+1
-	 	andlw 0xff-1
-	 	iorwf timerw+2, w
-	 	btfsc STATUS, Z
-	 		bsf ShutterState, SHUTTER_HALFWAY
-	
-	goto DoneInterrupt
-
-ExposureCountdown
-		movlw exposurew
-		call BD3Dec
-	
-		movlw exposurew
-		call BD3Test
-		btfss STATUS, Z
-			goto DoneInterrupt
-			
-		
-		clrf ShutterState
-		bcf TimerMode, 0
-		
-		Copy3 exposurew, exposure
-	 	
-		movlw timerw
-		movwf DisplayPointer
-
-	 	
-DoneInterrupt
-	bcf PIR1, TMR2IF
-	return
-
 main
 	PICINIT
-
 	MYINIT
 
-
 loop
-
-
 	bcf INTCON, GIE
 	call UpdateDisplay
-	
 	bsf INTCON, GIE
 	
 	call FrameDelay
-	btfsc STATUS, Z
-		call DoThinking
+	btfss STATUS, Z
+		goto loop
+		
 	
+	; Main logic
+	
+	movfw Buttons
+	xorwf OldButtons, f
+	btfss STATUS, Z
+		goto PassButton
+	
+	decfsz HoldDelay, f
+		goto NoButtons
+		
+	movlw BUTTON_REPEAT_DELAY
+	movwf HoldDelay
+	goto NoReDelay
+PassButton
+	movlw BUTTON_HOLD_DELAY
+	movwf HoldDelay
+NoReDelay
+
+	call ButtonMap
+
+NoButtons
+
+	movfw Buttons
+	movwf OldButtons
+
+	movfw DisplayPointer
+	btfss STATUS, Z
+		call DisplayTime
+
 	goto loop
 
-UpdateDisplay
-	incf DisplayLoop, f
-	movlw 5
-	xorwf DisplayLoop, w
-	btfsc STATUS, Z
-		clrf DisplayLoop
-
-DoDisplay
-	movfw DisplayLoop
-	TableLookup
-	goto DisplayDigit0
-	goto DisplayDigit1
-	goto DisplayDigit2
-	goto DisplayDigit3
-	goto ReadButtons
-
-DisplayDigit0
-	showdigit Display+0 , 0
-	return;
-DisplayDigit1
-	showdigit Display+1 , 1
-	return;
-DisplayDigit2
-	showdigit Display+2 , 2
-	return;
-DisplayDigit3
-	showdigit Display+3 , 3
-	return;
-	
-	
 	end
 	
